d1cc0838d896fc20f654d0f48605b6cadea7e96a
sirbobz <bobcertified@gmail.com>
2018-07-17 17:57:02 +0300

    updated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,7 +24,7 @@
 <form action="{{ url('uploadfile') }}" method="post" enctype="multipart/form-data">
 @csrf
 <div class="form-group">
-<input type="file" class="form-control-file" name="fileToUpload" id="exampleInputFile" aria-describedby="fileHelp">
+<input type="file" class="form-control-file" name="file" id="uploadInputFile" aria-describedby="fileHelp">
 <small id="fileHelp" class="form-text text-muted">Please upload a valid image file. Size of image should not be more than 2MB.</small>
 </div>
 <button type="submit" class="btn btn-primary">Submit</button>
