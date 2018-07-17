9641ce169fa4963bb6a08cc8e7f98b9b8944a390
sirbobz <bobcertified@gmail.com>
2018-07-17 17:42:36 +0300

    updated the file upload view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,32 +1,36 @@
 @extends('layouts.app')
-
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-                @if ($errors->any())
-                    <div class="alert alert-danger">
-                        <ul>
-                            @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
-                @endif
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="row justify-content-center">
+<div class="card">
+<div class="card-header">Upload File Example</div>
+<div class="card-body">
+@if ($message = Session::get('success'))
+<div class="alert alert-success alert-block">
+<button type="button" class="close" data-dismiss="alert">×</button>
+<strong>{{ $message }}</strong>
+</div>
+@endif
+@if (count($errors) > 0)
+<div class="alert alert-danger">
+<strong>Whoops!</strong> There were some problems with your input.<br><br>
+<ul>
+@foreach ($errors->all() as $error)
+<li>{{ $error }}</li>
+@endforeach
+</ul>
+</div>
+@endif
+<form action="{{ url('uploadfile') }}" method="post" enctype="multipart/form-data">
+@csrf
+<div class="form-group">
+<input type="file" class="form-control-file" name="fileToUpload" id="exampleInputFile" aria-describedby="fileHelp">
+<small id="fileHelp" class="form-text text-muted">Please upload a valid image file. Size of image should not be more than 2MB.</small>
+</div>
+<button type="submit" class="btn btn-primary">Submit</button>
+</form>
+</div>
+</div>
+</div>
 </div>
 @endsection
